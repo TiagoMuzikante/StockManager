208bd5378c57ee45c1c2146867c21c0ec5219608
Minerator123 <tiagommuzikante@hotmail.com>
2025-05-26 13:21:37 -0300

    fix controllers/implementController - add requestMapping

--- src/main/java/com/lostdev/StockManager/controller/ImplementController.java
+++ src/main/java/com/lostdev/StockManager/controller/ImplementController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/implements")
+@RequestMapping("/implement")
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
