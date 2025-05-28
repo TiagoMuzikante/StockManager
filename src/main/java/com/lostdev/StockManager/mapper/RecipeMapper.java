e30769ff30d7ddb2054bd5429cabbf7203acb5aa
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-05-27 22:41:05 -0300

    fix mapper/recipeMapper - add toBasic and remove idToImplement

--- src/main/java/com/lostdev/StockManager/mapper/RecipeMapper.java
+++ src/main/java/com/lostdev/StockManager/mapper/RecipeMapper.java
@@ -1,39 +1,34 @@
 package com.lostdev.StockManager.mapper;
 
+import com.lostdev.StockManager.DTOs.recipe.RecipeBasicDTO;
 import com.lostdev.StockManager.DTOs.recipe.RecipePostDTO;
 import com.lostdev.StockManager.DTOs.utils.ProductItemDTO;
-import com.lostdev.StockManager.domain.stock.Implement;
 import com.lostdev.StockManager.domain.stock.Recipe;
 import com.lostdev.StockManager.domain.utils.ProductItem;
-import com.lostdev.StockManager.service.ImplementService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ProductItemMapper.class)
 public abstract class RecipeMapper {
 
+  @Autowired
   protected ProductItemMapper productItemMapper;
-  protected ImplementService implementService;
 
-  @Mapping(target = "productItems", qualifiedByName = "toProductItemList")
-  @Mapping(source = "implementId", target = "implement", qualifiedByName = "toImplement")
+  public abstract RecipeBasicDTO toBasic(Recipe recipe);
+
+  @Mapping(source = "productItems", target = "productItems", qualifiedByName = "toProductItemList")
   public abstract Recipe toRecipe(RecipePostDTO recipePostDTO);
 
   @Named("toProductItemList")
-  protected List<ProductItem> toProductItemList(List<ProductItemDTO> productItemDTOS){
-    return productItemDTOS
+  protected List<ProductItem> toProductItemList(List<ProductItemDTO> productItems){
+    return productItems
         .stream()
         .map(productItemMapper::toProductItem)
         .collect(Collectors.toList());
   }
-
-  @Named("toImplement")
-  protected Implement toImplement(Long id){
-    return implementService.findById(id);
-  }
-
 }
