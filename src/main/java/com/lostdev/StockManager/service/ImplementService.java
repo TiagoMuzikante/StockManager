db1cf7dd649f66e5bcbe96ea0625428750db1d05
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-05-27 22:35:14 -0300

    fix service/implementService - change response entity to DTO

--- src/main/java/com/lostdev/StockManager/service/ImplementService.java
+++ src/main/java/com/lostdev/StockManager/service/ImplementService.java
@@ -1,13 +1,16 @@
 package com.lostdev.StockManager.service;
 
+import com.lostdev.StockManager.DTOs.Implement.ImplementBasicDTO;
 import com.lostdev.StockManager.DTOs.Implement.ImplementPostDTO;
 import com.lostdev.StockManager.domain.stock.Implement;
+import com.lostdev.StockManager.domain.stock.Recipe;
 import com.lostdev.StockManager.mapper.ImplementMapper;
 import com.lostdev.StockManager.repository.ImplementRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,18 +18,25 @@ public class ImplementService {
 
   private final ImplementRepository implementRepository;
   private final ImplementMapper implementMapper;
+  private final RecipeService recipeService;
 
   public Implement save(ImplementPostDTO implementPostDTO){
-    return implementRepository.save(implementMapper.toImplement(implementPostDTO));
+    Recipe recipe = recipeService.save(implementPostDTO.getRecipe());
+    Implement implement = implementMapper.toImplement(implementPostDTO);
+    implement.setRecipe(recipe);
+    return implementRepository.save(implement);
   }
 
-  public List<Implement> listAll(){
-    return implementRepository.findAll();
+  public List<ImplementBasicDTO> listAll(){
+    return implementRepository.findAll()
+        .stream()
+        .map(implementMapper::toBasic)
+        .collect(Collectors.toList());
   }
 
-  public Implement findById(Long id){
-    return implementRepository.findById(id)
-        .orElseThrow(() -> new RuntimeException("Nenhum resultado encontrado."));
+  public ImplementBasicDTO findById(Long id){
+    return implementMapper.toBasic(implementRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Nenhum resultado encontrado.")));
   }
 
 }
