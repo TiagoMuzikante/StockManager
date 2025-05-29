763c415dcb552bc5caf795a8f2dd42b580bfbb56
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-05-29 17:20:55 -0300

    feat - enums/buy/ - create reason and status enum

---
+++ src/main/java/com/lostdev/StockManager/enums/buy/Reason.java
@@ -0,0 +1,6 @@
+package com.lostdev.StockManager.enums.buy;
+
+public enum Reason {
+  PEDIDO_PRODUCAO,
+  RESERVA_TECNICA
+}
---
+++ src/main/java/com/lostdev/StockManager/enums/buy/Status.java
@@ -0,0 +1,9 @@
+package com.lostdev.StockManager.enums.buy;
+
+public enum Status {
+  PENDENTE_DE_COMPRA,
+  AGUARDANDO_ENTREGA,
+  AGUARDANDO_CONFERENCIA,
+  AGUARDANDO_ESTOCAGEM,
+  COMCLUIDO
+}
