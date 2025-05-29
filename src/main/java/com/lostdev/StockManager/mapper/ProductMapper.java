a0254099aa99b54301aeb07e8f21359124ad6d28
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-05-29 17:26:57 -0300

    fix mapper/productMapper - implement productNoReference mapper

--- src/main/java/com/lostdev/StockManager/mapper/ProductMapper.java
+++ src/main/java/com/lostdev/StockManager/mapper/ProductMapper.java
@@ -3,6 +3,7 @@ package com.lostdev.StockManager.mapper;
 import com.lostdev.StockManager.domain.Supplier;
 import com.lostdev.StockManager.domain.stock.Product;
 import com.lostdev.StockManager.dtos.product.ProductBasicDTO;
+import com.lostdev.StockManager.dtos.product.ProductNoReferenceDTO;
 import com.lostdev.StockManager.dtos.product.ProductPostDTO;
 import com.lostdev.StockManager.dtos.product.ProductPutDTO;
 import com.lostdev.StockManager.service.SupplierService;
@@ -14,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = SupplierMapper.class)
 public abstract class ProductMapper {
 
   @Autowired
@@ -23,12 +24,22 @@ public abstract class ProductMapper {
   @Mapping(target = "availableAmount", expression = "java(0)")
   @Mapping(target = "reservedAmount", expression = "java(0)")
   @Mapping(target = "pendingAmount", expression = "java(0)")
+  @Mapping(target = "minimumAmount", expression = "java(0)")
   @Mapping(source = "supplierId", target = "supplier", qualifiedByName = "idToSupplier")
   public abstract Product toProduct(ProductPostDTO productPostDTO);
 
   public abstract Product toProduct(ProductPutDTO productPutDTO);
 
   public abstract ProductBasicDTO toProductBasic(Product product);
+
+  public abstract ProductNoReferenceDTO toNoReferenceDTO(Product product);
+
+  public List<ProductNoReferenceDTO> toNoReferenceList(List<Product> products){
+    return products
+        .stream()
+        .map(this::toNoReferenceDTO)
+        .collect(Collectors.toList());
+  }
 
   public List<ProductBasicDTO> toProductBasicList(List<Product> products){
     return products.stream()
@@ -38,7 +49,7 @@ public abstract class ProductMapper {
 
   @Named("idToSupplier")
   protected Supplier idToSupplier(Long supplierId){
+    if(supplierId == null) return null;
     return supplierService.findById(supplierId);
   }
-
 }
