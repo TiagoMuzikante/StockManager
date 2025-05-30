d101467e2f47f0ad1a93d221b823df2223b33012
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-05-29 21:07:50 -0300

    fix domain/listener/productOutListener - comment incomplete code

--- src/main/java/com/lostdev/StockManager/domain/listener/ProductOutListener.java
+++ src/main/java/com/lostdev/StockManager/domain/listener/ProductOutListener.java
@@ -6,22 +6,22 @@ import com.lostdev.StockManager.domain.stock.ProductOut;
 import com.lostdev.StockManager.enums.buy.Reason;
 import com.lostdev.StockManager.service.BuyOrderService;
 import jakarta.persistence.PostPersist;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 
-@RequiredArgsConstructor
 public class ProductOutListener {
 
-  private final BuyOrderService buyOrderService;
+  public static BuyOrderService buyOrderService;
 
-  @PostPersist
-  public void restockMinimal(ProductOut productOut){
-    Product product = productOut.getProduct();
-    if(product.getAvailableAmount() < product.getMinimumAmount()){
-      int pendingAmount = product.getMinimumAmount() - product.getAvailableAmount();
-      BuyOrder buyOrder = new BuyOrder(pendingAmount, Reason.RESERVA_TECNICA, product);
-      buyOrderService.save(buyOrder);
-    }
-  }
+//  @PostPersist
+//  public void restockMinimal(@Lazy ProductOut productOut){
+//    Product product = productOut.getProduct();
+//    if(product.getAvailableAmount() < product.getMinimumAmount()){
+//      int pendingAmount = product.getMinimumAmount() - product.getAvailableAmount();
+//      BuyOrder buyOrder = new BuyOrder(pendingAmount, Reason.RESERVA_TECNICA, product);
+//      buyOrderService.save(buyOrder);
+//    }
+//  }
+
+
 
 }
-  
