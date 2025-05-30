fd00a0f3dc17859f303be5b91be96c30bf0fe308
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-05-29 21:04:43 -0300

    fix dtos/prodict/productBasicDTO - add brand attribute

--- src/main/java/com/lostdev/StockManager/dtos/product/ProductBasicDTO.java
+++ src/main/java/com/lostdev/StockManager/dtos/product/ProductBasicDTO.java
@@ -11,6 +11,7 @@ public class ProductBasicDTO {
 
   private Long id;
   private String name;
+  private String brand;
   private Integer availableAmount;
   private Integer reservedAmount;
   private Integer pendingAmount;
