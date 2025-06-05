858e93419060ff9d946084cb4091bacb676d3a6a
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 20:01:00 -0300

    fix controllers/product - fix routes and add new endPoints

--- src/main/java/com/lostdev/StockManager/controller/ProductController.java
+++ src/main/java/com/lostdev/StockManager/controller/ProductController.java
@@ -4,7 +4,8 @@ import com.lostdev.StockManager.domain.stock.Product;
 import com.lostdev.StockManager.dtos.product.ProductBasicDTO;
 import com.lostdev.StockManager.dtos.product.ProductNoReferenceDTO;
 import com.lostdev.StockManager.dtos.product.ProductPostDTO;
-import com.lostdev.StockManager.service.ProductService;
+import com.lostdev.StockManager.dtos.product.ProductPutDTO;
+import com.lostdev.StockManager.service.domainService.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,30 +16,41 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/product")
+@RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
 
   private final ProductService productService;
 
-  @PostMapping
+  @PostMapping("/new")
   public ResponseEntity<ProductBasicDTO> save(@RequestBody ProductPostDTO productPostDTO){
     return new ResponseEntity<>(productService.save(productPostDTO), HttpStatus.CREATED);
   }
 
-  @GetMapping
-  public ResponseEntity<List<ProductBasicDTO>> listAllBasic(){
-    return ResponseEntity.ok(productService.listAllBasic());
+  @GetMapping("/new")
+  public ResponseEntity<ProductPostDTO> blankPattern(){
+    return ResponseEntity.ok(new ProductPostDTO());
   }
 
-  @GetMapping("/all")
+  @GetMapping()
   public ResponseEntity<List<ProductNoReferenceDTO>> listAll(){
     return ResponseEntity.ok(productService.listAll());
   }
 
-  @GetMapping("/{id}")
+  @GetMapping("/{id}/show")
   public ResponseEntity<Product> showProduct(@PathVariable Long id){
     return new ResponseEntity<>(productService.findById(id), HttpStatus.OK);
+  }
+
+  @PutMapping("/{id}/edit")
+  public ResponseEntity<Product> replace(@PathVariable Long id, @RequestBody ProductPutDTO productPutDTO){
+    return ResponseEntity.ok(productService.replace(id, productPutDTO));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> destroy(@PathVariable Long id){
+    productService.destroy(id);
+    return ResponseEntity.noContent().build();
   }
 
 
