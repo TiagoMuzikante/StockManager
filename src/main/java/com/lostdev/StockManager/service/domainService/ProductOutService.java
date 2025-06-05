93210fdd1482630e4ba17a19cd4d89ed4a4a847e
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 20:11:30 -0300

    fix service/productOutService - turn concise service

--- src/main/java/com/lostdev/StockManager/service/domainService/ProductOutService.java
+++ src/main/java/com/lostdev/StockManager/service/domainService/ProductOutService.java
@@ -1,13 +1,11 @@
-package com.lostdev.StockManager.service;
+package com.lostdev.StockManager.service.domainService;
 
 import com.lostdev.StockManager.domain.stock.ProductOut;
 import com.lostdev.StockManager.mapper.ProductOutMapper;
 import com.lostdev.StockManager.repository.ProductOutRepository;
 import com.lostdev.StockManager.dtos.productOut.ProductOutBasicDTO;
-import com.lostdev.StockManager.dtos.productOut.ProductOutPostDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -31,11 +29,8 @@ public class ProductOutService {
     return productOutRepository.findById(id).orElseThrow(() -> new RuntimeException("Nenhum resultado encontrado."));
   }
 
-  @Transactional
-  public ProductOutBasicDTO save(ProductOutPostDTO productOutPostDTO){
-    ProductOut productOut = productOutMapper.toProductOut(productOutPostDTO);
-    productService.decrementAmount(productOut.getProduct(), productOut.getAmount());
-    return productOutMapper.toBasic(productOutRepository.save(productOut));
+  public ProductOut save(ProductOut productOut){
+    return productOutRepository.save(productOut);
   }
 
 }
