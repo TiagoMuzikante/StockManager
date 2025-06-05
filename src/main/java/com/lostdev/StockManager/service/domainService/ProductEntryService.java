678a8518723987ea9ab4c059a7d3d10b01736bda
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 20:03:21 -0300

    fix service/productEntryService - turn concise service

--- src/main/java/com/lostdev/StockManager/service/domainService/ProductEntryService.java
+++ src/main/java/com/lostdev/StockManager/service/domainService/ProductEntryService.java
@@ -1,13 +1,11 @@
-package com.lostdev.StockManager.service;
+package com.lostdev.StockManager.service.domainService;
 
 import com.lostdev.StockManager.domain.stock.ProductEntry;
 import com.lostdev.StockManager.mapper.ProductEntryMapper;
 import com.lostdev.StockManager.repository.ProductEntryRepository;
 import com.lostdev.StockManager.dtos.productEntry.ProductEntryBasicDTO;
-import com.lostdev.StockManager.dtos.productEntry.ProductEntryPostDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,11 +17,8 @@ public class ProductEntryService {
   private final ProductService productService;
   private final ProductEntryMapper productEntryMapper;
 
-  @Transactional
-  public ProductEntryBasicDTO save(ProductEntryPostDTO productEntryPostDTO){
-    ProductEntry productEntry = productEntryMapper.toProductEntry(productEntryPostDTO);
-    productService.incrementAmount(productEntry.getProduct(),productEntry.getAmount());
-    return productEntryMapper.toBasic(productEntryRepository.save(productEntry));
+  public ProductEntry save(ProductEntry productEntry){
+    return productEntryRepository.save(productEntry);
   }
 
   public ProductEntryBasicDTO findById(Long id){
