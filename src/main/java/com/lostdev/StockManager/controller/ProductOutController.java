99cd67198eae094ca3a6c841b4f61511c4134576
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 20:08:13 -0300

    fix controllers/ProductOutController - inactive post endpoint

--- src/main/java/com/lostdev/StockManager/controller/ProductOutController.java
+++ src/main/java/com/lostdev/StockManager/controller/ProductOutController.java
@@ -3,7 +3,7 @@ package com.lostdev.StockManager.controller;
 import com.lostdev.StockManager.domain.stock.ProductOut;
 import com.lostdev.StockManager.dtos.productOut.ProductOutBasicDTO;
 import com.lostdev.StockManager.dtos.productOut.ProductOutPostDTO;
-import com.lostdev.StockManager.service.ProductOutService;
+import com.lostdev.StockManager.service.domainService.ProductOutService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +19,10 @@ public class ProductOutController {
 
   private final ProductOutService productOutService;
 
-  @PostMapping
-  public ResponseEntity<ProductOutBasicDTO> save(@RequestBody ProductOutPostDTO productOutPostDTO){
-    return new ResponseEntity<ProductOutBasicDTO>(productOutService.save(productOutPostDTO), HttpStatus.CREATED);
-  }
+//  @PostMapping
+//  public ResponseEntity<ProductOutBasicDTO> save(@RequestBody ProductOutPostDTO productOutPostDTO){
+//    return new ResponseEntity<ProductOutBasicDTO>(productOutService.save(productOutPostDTO), HttpStatus.CREATED);
+//  }
 
   @GetMapping()
   public ResponseEntity<List<ProductOutBasicDTO>> listAll(){
