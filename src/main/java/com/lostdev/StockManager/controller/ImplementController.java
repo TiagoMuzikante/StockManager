c1236755ffc2920f5b165136283a1db4034813cc
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 19:55:26 -0300

    fix controllers/implementController - inactive implement post endpoint

--- src/main/java/com/lostdev/StockManager/controller/ImplementController.java
+++ src/main/java/com/lostdev/StockManager/controller/ImplementController.java
@@ -3,7 +3,7 @@ package com.lostdev.StockManager.controller;
 import com.lostdev.StockManager.dtos.Implement.ImplementBasicDTO;
 import com.lostdev.StockManager.dtos.Implement.ImplementPostDTO;
 import com.lostdev.StockManager.domain.Implement;
-import com.lostdev.StockManager.service.ImplementService;
+import com.lostdev.StockManager.service.domainService.ImplementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +19,10 @@ public class ImplementController {
 
   private final ImplementService implementService;
 
-  @PostMapping
-  public ResponseEntity<Implement> save(@RequestBody ImplementPostDTO implementPostDTO){
-    return new ResponseEntity<>(implementService.save(implementPostDTO), HttpStatus.CREATED);
-  }
+//  @PostMapping
+//  public ResponseEntity<Implement> save(@RequestBody ImplementPostDTO implementPostDTO){
+//    return new ResponseEntity<>(implementService.save(implementPostDTO), HttpStatus.CREATED);
+//  }
 
   @GetMapping
   public ResponseEntity<List<ImplementBasicDTO>> listAll(){
