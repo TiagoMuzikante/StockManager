0cecce99a521e77e902d98db9018e53fe5b9be5e
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 20:06:37 -0300

    fix dto/product/productNoReferenceDTO - add new attributes

--- src/main/java/com/lostdev/StockManager/dtos/product/ProductNoReferenceDTO.java
+++ src/main/java/com/lostdev/StockManager/dtos/product/ProductNoReferenceDTO.java
@@ -1,6 +1,9 @@
 package com.lostdev.StockManager.dtos.product;
 
 import com.lostdev.StockManager.dtos.supplier.SupplierBasicDTO;
+import com.lostdev.StockManager.enums.product.FormType;
+import com.lostdev.StockManager.enums.product.HoseAdapterPoint;
+import com.lostdev.StockManager.enums.product.ScrewNutsFlange;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,13 +17,31 @@ public class ProductNoReferenceDTO {
 
   private Long id;
   private String name;
-  private String address;
-  private Boolean noAddress;
   private String brand;
-  private Integer availableAmount = 0;
-  private Integer reservedAmount = 0;
-  private Integer pendingAmount = 0;
-  private Integer minimumAmount = 0;
+  private String  form_type;
+  private String mold;
+  private String type;
+  private Integer width_cm;
+  private Integer length_cm;
+  private Integer inner_diameter;
+  private Integer outer_diameter;
+  private String depth;
+  private HoseAdapterPoint point;
+  private Integer jic_mm;
+  private Integer opened_depth;
+  private Integer closed_depth;
+  private Integer average_delivery_time_days;
+  private ScrewNutsFlange flange;
+  private String material;
+  private String hardness;
+  private String address;
+  private Boolean no_address;
+  private Integer availableAmount;
+  private Integer reservedAmount;
+  private Integer pendingAmount;
+  private Integer minimumAmount;
+  private Integer price_in_cents;
+  private Double price_variation_percent;
 
   private SupplierBasicDTO supplier;
 
