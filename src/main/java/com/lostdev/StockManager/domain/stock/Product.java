fbe04984a7796635a0290911b75aff35c7980653
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 19:59:31 -0300

    fix domain/stock/product - add attributes

--- src/main/java/com/lostdev/StockManager/domain/stock/Product.java
+++ src/main/java/com/lostdev/StockManager/domain/stock/Product.java
@@ -1,6 +1,9 @@
 package com.lostdev.StockManager.domain.stock;
 
 import com.lostdev.StockManager.domain.Supplier;
+import com.lostdev.StockManager.enums.product.FormType;
+import com.lostdev.StockManager.enums.product.HoseAdapterPoint;
+import com.lostdev.StockManager.enums.product.ScrewNutsFlange;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,13 +20,33 @@ public class Product {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
+  private String brand;
+  private FormType formType;
+  private String mold;
+  private String type;
+  private Integer widthCm;
+  private Integer lengthCm;
+  private Integer innerDiameter;
+  private Integer outerDiameter;
+  private String depth;
+  private HoseAdapterPoint point;
+  private Integer jicMm;
+  private Integer openedDepth;
+  private Integer closedDepth;
+  private Integer averageDeliveryTimeDays;
+  private ScrewNutsFlange flange;
+  private String material;
+  private String hardness;
   private String address;
   private Boolean noAddress;
-  private String brand;
   private Integer availableAmount = 0;
   private Integer reservedAmount = 0;
   private Integer pendingAmount = 0;
   private Integer minimumAmount = 0;
+  private Integer priceInCents;
+  private Double priceVariationPercent;
+
+
 
   @ManyToOne
   @JoinColumn(name = "supplier_id")
