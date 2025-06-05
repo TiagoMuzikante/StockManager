899ed6fe22646488125941862cd3d5b0a2dea85d
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 20:12:19 -0300

    fix dtos/product/productPutDTO - add new attributes

--- src/main/java/com/lostdev/StockManager/dtos/product/ProductPutDTO.java
+++ src/main/java/com/lostdev/StockManager/dtos/product/ProductPutDTO.java
@@ -12,10 +12,30 @@ public class ProductPutDTO {
   private Long id;
   private String name;
   private String address;
-  private Boolean noAddress;
+  private Boolean no_address;
   private String brand;
-  private Integer amount;
-  private Integer minimunAmount;
-  private Integer itensPerPackage;
+  private String form_type;
+  private String mold;
+  private String type;
+  private Integer width_cm;
+  private Integer length_cm;
+  private Integer inner_diameter;
+  private Integer outer_diameter;
+  private String depth;
+  private String point;
+  private Integer jic_mm;
+  private Integer opened_depth;
+  private Integer closed_depth;
+  private String flange;
+  private String material;
+  private String hardness;
+  private Integer availableAmount;
+  private Integer reservedAmount;
+  private Integer pendingAmount;
+  private Integer minimumAmount;
+  private Integer price_in_cents;
+  private Double price_variation_percent;
+
+  private String supplier;
 
 }
