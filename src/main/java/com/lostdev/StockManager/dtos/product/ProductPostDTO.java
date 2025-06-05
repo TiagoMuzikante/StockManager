fb6794332764476af41e85268a53d8c0878b7315
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-06-05 20:12:08 -0300

    fix dtos/product/productPostDTO - add new attributes

--- src/main/java/com/lostdev/StockManager/dtos/product/ProductPostDTO.java
+++ src/main/java/com/lostdev/StockManager/dtos/product/ProductPostDTO.java
@@ -11,10 +11,27 @@ public class ProductPostDTO {
 
   private String name;
   private String address;
-  private Boolean noAddress;
+  private Boolean no_address;
   private String brand;
-  private Integer minimumAmount;
+  private Integer minimum_amount;
+  private String form_type;
+  private String mold;
+  private String type;
+  private Integer width_cm;
+  private Integer length_cm;
+  private Integer inner_diameter;
+  private Integer outer_diameter;
+  private String depth;
+  private String point;
+  private Integer jic_mm;
+  private Integer opened_depth;
+  private Integer closed_depth;
+  private String flange;
+  private String material;
+  private String hardness;
+  private Integer price_in_cents;
+  private Double price_variation_percent;
 
-  private Long supplierId;
+  private String supplier;
 
 }
